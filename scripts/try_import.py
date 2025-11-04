f130e3192cfba8cea852d1440014b71ce969a72c
osamaaraja <osamaakbarraja@yahoo.com>
2025-11-04 20:14:05 +0100

    detecting pads called

--- scripts/try_import.py
+++ scripts/try_import.py
@@ -1,2 +1,6 @@
 import pcb_partgraph
 print(pcb_partgraph.ping())
+from pcb_partgraph.detect import detect_pads
+pads = detect_pads("../outputs/scene_v1.png")  # or your latest image path
+print("pads found:", len(pads))
+print(pads)
